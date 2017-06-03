d278e92da247893c21fd0c9e7b7b4cab43fd4c2a
Kontinuation <morisama@yeah.net>
2017-06-03 19:04:03 +0800

    moved code to collect variables in expression to a new file

---
+++ collect_vars.hh
@@ -0,0 +1,54 @@
+#ifndef _COLLECT_VARS_H_
+#define _COLLECT_VARS_H_
+
+// Traverse to find out all variables in expression
+
+#include "expr.hh"
+#include <map>
+#include <set>
+
+namespace ddf {
+
+template <typename numeric_type>
+class collect_variable : public math_expr_visitor<numeric_type> {
+public:
+    virtual void apply(constant<numeric_type> *expr) {
+        // ignore
+    }
+    virtual void apply(identity<numeric_type> *expr) {
+        // ignore
+    }
+    
+    virtual void apply(variable<numeric_type> *expr) {
+        _vars[expr->_var] = expr;
+    }
+    
+    virtual void apply(function_call<numeric_type> *expr) {
+        for (auto &arg: expr->_args) {
+            arg->apply(this);
+        }
+    }
+    
+    virtual void apply(dfunction_call<numeric_type> *expr) {
+        expr->_d_arg->apply(this);
+        for (auto &arg: expr->_args) {
+            arg->apply(this);
+        }
+    }
+    
+    virtual void apply(addition<numeric_type> *expr) {
+        expr->_a->apply(this);
+        expr->_b->apply(this);
+    }
+
+    std::map<std::string, variable<numeric_type> *> &vars(void) {
+        return _vars;
+    }
+    
+private:
+    std::map<std::string, variable<numeric_type> *> _vars;
+};
+
+} // end namespace ddf
+
+#endif /* _COLLECT_VARS_H_ */
--- train.hh
+++ train.hh
@@ -10,51 +10,9 @@
 // the training loss, and hope the trained network to be general enough to
 // solve such problem.
 
-#include "expr.hh"
-#include <map>
-#include <set>
+#include "collect_vars.hh"
 
 namespace ddf {
-
-template <typename numeric_type>
-class collect_variable : public math_expr_visitor<numeric_type> {
-public:
-    virtual void apply(constant<numeric_type> *expr) {
-        // ignore
-    }
-    virtual void apply(identity<numeric_type> *expr) {
-        // ignore
-    }
-    
-    virtual void apply(variable<numeric_type> *expr) {
-        _vars[expr->_var] = expr;
-    }
-    
-    virtual void apply(function_call<numeric_type> *expr) {
-        for (auto &arg: expr->_args) {
-            arg->apply(this);
-        }
-    }
-    
-    virtual void apply(dfunction_call<numeric_type> *expr) {
-        expr->_d_arg->apply(this);
-        for (auto &arg: expr->_args) {
-            arg->apply(this);
-        }
-    }
-    
-    virtual void apply(addition<numeric_type> *expr) {
-        expr->_a->apply(this);
-        expr->_b->apply(this);
-    }
-
-    std::map<std::string, variable<numeric_type> *> &vars(void) {
-        return _vars;
-    }
-    
-private:
-    std::map<std::string, variable<numeric_type> *> _vars;
-};
 
 template <typename numeric_type>
 class optimization {
