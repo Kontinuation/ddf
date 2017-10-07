04c4132110515fb7653933149c17a78305e9933b
Kontinuation <morisama@yeah.net>
2017-10-07 22:13:41 +0800

    added 1 layer liear model as baseline

--- src/main.cc
+++ src/main.cc
@@ -17,7 +17,51 @@ void show_fea_as_image(const float *fea, int w, int h, float threshold = -0.2) {
 volatile bool g_signal_quit = false;
 
 template <typename numeric_type>
-ddf::math_expr<numeric_type> *fc_model(
+ddf::math_expr<numeric_type> *fc_1_model(
+    const ddf::matrix<numeric_type> &xs, const ddf::matrix<numeric_type> &ls)
+{
+    // -- identify problem size --
+    int dimension = xs.shape(1);
+    int n_classes = ls.shape(1);
+        
+    // -- prepare deep model --
+    int len_w0 = n_classes * dimension;
+    int len_b0 = n_classes;
+
+    ddf::variable<numeric_type> *var_w0 =
+        new ddf::variable<numeric_type>("w0", ddf::vector<numeric_type>(len_w0));
+    ddf::variable<numeric_type> *var_b0 =
+        new ddf::variable<numeric_type>("b0", ddf::vector<numeric_type>(len_b0));
+
+    // initial value of hyper parameters
+    var_w0->value().fill_rand();
+    var_b0->value().fill_rand();
+        
+    ddf::variable<numeric_type> *var_x = 
+        new ddf::variable<numeric_type>("x", ddf::vector<numeric_type>(dimension));
+    ddf::variable<numeric_type> *var_l = 
+        new ddf::variable<numeric_type>("l", ddf::vector<numeric_type>(n_classes));
+
+    // predict: w0 * x + b0
+    auto matmul_0 = new ddf::matrix_mult<numeric_type>();
+    auto predict = new ddf::addition<numeric_type>(
+        new ddf::function_call<numeric_type>(
+            matmul_0,
+            var_w0, 
+            var_x),
+        var_b0);
+
+    // loss: DS(predict, l)
+    auto DS = new ddf::softmax_cross_entropy_with_logits<numeric_type>();
+    auto loss = new ddf::function_call<numeric_type>(
+        DS, predict, var_l);
+
+    return loss;
+}
+
+
+template <typename numeric_type>
+ddf::math_expr<numeric_type> *fc_2_model(
     const ddf::matrix<numeric_type> &xs, const ddf::matrix<numeric_type> &ls, int n_hidden)
 {
     // -- identify problem size --
@@ -51,8 +95,6 @@ ddf::math_expr<numeric_type> *fc_model(
         new ddf::variable<numeric_type>("l", ddf::vector<numeric_type>(n_classes));
 
     // predict: w1 * (relu(w0 * x + b0)) + b1
-    
-    // ddf::matrix_mult<numeric_type> matmul_0, matmul_1;
     auto matmul_0 = new ddf::matrix_mult<numeric_type>();
     auto matmul_1 = new ddf::matrix_mult<numeric_type>();
     auto relu_0 = new ddf::relu<numeric_type>();
@@ -158,6 +200,7 @@ ddf::math_expr<numeric_type> *conv_model(
         new ddf::variable<numeric_type>(
             "w", ddf::vector<numeric_type>(100));
 
+
     // initial value of hyper parameters
     var_c0->value().fill_rand(-0.5, 0.5);
     var_b0->value().fill(0);
@@ -171,32 +214,36 @@ ddf::math_expr<numeric_type> *conv_model(
     auto relu_1 = new ddf::relu<numeric_type>();
     auto relu_2 = new ddf::relu<numeric_type>();
     
-    // predict: conv0 -> pool0 -> conv1 -> pool1 -> conv2 -> pool2
+    // predict:
+    //   conv0 -> relu0 -> pool0
+    //   -> conv1 -> relu1 -> pool1
+    //   -> conv2 -> relu2 -> pool2
+    //   -> fc
     ddf::math_expr<numeric_type> *predict =
-        // new ddf::function_call<numeric_type>(
-        //     fc,
-        //     var_w,
         new ddf::function_call<numeric_type>(
-            pool_2,
+            fc,
+            var_w,
             new ddf::function_call<numeric_type>(
-                relu_2,
+                pool_2,
                 new ddf::function_call<numeric_type>(
-                    conv_2,
+                    relu_2,
                     new ddf::function_call<numeric_type>(
-                        pool_1,
+                        conv_2,
                         new ddf::function_call<numeric_type>(
-                            relu_1,
+                            pool_1,
                             new ddf::function_call<numeric_type>(
-                                conv_1,
+                                relu_1,
                                 new ddf::function_call<numeric_type>(
-                                    pool_0, 
+                                    conv_1,
                                     new ddf::function_call<numeric_type>(
-                                        relu_0, 
+                                        pool_0, 
                                         new ddf::function_call<numeric_type>(
-                                            conv_0,
-                                            var_x, var_c0, var_b0))),
-                                var_c1, var_b1))),
-                    var_c2, var_b2)));
+                                            relu_0, 
+                                            new ddf::function_call<numeric_type>(
+                                                conv_0,
+                                                var_x, var_c0, var_b0))),
+                                    var_c1, var_b1))),
+                        var_c2, var_b2))));
         
     // loss: DS(predict, l)
     auto DS = new ddf::softmax_cross_entropy_with_logits<numeric_type>();
@@ -247,7 +294,8 @@ int main(int argc, char *argv[]) {
             dimension, n_samples, n_classes);
 
         // -- train this model using optimizer defined in train.hh --
-        // auto loss = std::unique_ptr<ddf::math_expr<float> >(fc_model(xs, ls, 20));
+        // auto loss = std::unique_ptr<ddf::math_expr<float> >(fc_1_model(xs, ls));
+        // auto loss = std::unique_ptr<ddf::math_expr<float> >(fc_2_model(xs, ls, 20));
         auto loss = std::unique_ptr<ddf::math_expr<float> >(conv_model(xs, ls));
         
         // construct optimizer
@@ -264,7 +312,7 @@ int main(int argc, char *argv[]) {
 
         // perform iterative optimization to reduce training loss
         optimizer.set_learning_rate(alpha);
-        for (int iter = 0; iter < 1000; iter++) {
+        for (int iter = 0; iter < 100000; iter++) {
             clock_t start = clock();
             optimizer.step(1);
             clock_t end = clock();
