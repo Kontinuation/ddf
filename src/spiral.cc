51416420bd3be0a08f8b391d13bc487d41d36a7a
Kontinuation <morisama@yeah.net>
2017-10-08 02:48:46 +0800

    added an accompany python notebook for plotting spiral data

--- src/spiral.cc
+++ src/spiral.cc
@@ -9,8 +9,12 @@ int main(int argc, char *argv[]) {
     int N = 100;
     int K = 3;
     double alpha = 0.1;
+    int n_hidden = 20;
     if (argc > 1) {
         alpha = atof(argv[1]);
+    }
+    if (argc > 2) {
+        n_hidden = atoi(argv[2]);
     }
 
     ddf::matrix<double> xs(N * K, 2);
@@ -22,11 +26,11 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < N; i++) {
             int ix = N * j + i;
             double r = i * r_step;
-            double t = j * 4 + i * r_step * 4 + ((double) rand() / RAND_MAX * 0.2);
+            double t = j * 4 + i * r_step * 4 + ((double) rand() / RAND_MAX * 0.5);
             xs(ix, 0) = r * std::sin(t);
             xs(ix, 1) = r * std::cos(t);
             ls(ix, j) = 1;
-            // printf("%f %f %d\n", xs(ix, 0), xs(ix, 1), j);
+            printf("data %f %f %d\n", xs(ix, 0), xs(ix, 1), j);
         }
     }
 
@@ -36,7 +40,7 @@ int main(int argc, char *argv[]) {
         new ddf::variable<double>("l", ddf::vector<double>(K));
     
     // auto predict = fc_1_model(var_x, var_l, xs, ls);
-    auto predict = fc_2_model(var_x, var_l, xs, ls, 20);
+    auto predict = fc_2_model(var_x, var_l, xs, ls, n_hidden);
 
     auto DS = new ddf::softmax_cross_entropy_with_logits<double>();
     auto loss = std::unique_ptr<ddf::math_expr<double> >(
@@ -65,6 +69,22 @@ int main(int argc, char *argv[]) {
         logging::info("iter: %d, loss: %f, cost: %f sec",
             iter, optimizer.loss(),
             (double)(end - start) / CLOCKS_PER_SEC);
+    }
+
+    // plot splitting regions as contours
+    ddf::vector<double> t;
+    int n_steps = 500;
+    double step = 3.0 / n_steps;
+    for (int i = 0; i < n_steps; i++) {
+        double x = -1.5 + step * i;
+        for (int j = 0; j < n_steps; j++) {
+            double y = -1.5 + step * j;
+            var_x->value()[0] = x;
+            var_x->value()[1] = y;
+            predict->eval(t);
+            int c = std::distance(&t[0], std::max_element(&t[0], &t[0] + K));
+            printf("prediction %f %f %d\n", x, y, c);
+        }
     }
 
     return 0;
